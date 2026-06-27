42058c5827988045174bf4e996ef6a2fde6b86ea
nzeenath <nzeenath2002@gmail.com>
2026-06-27 14:55:47 +0530

    2nd commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+console.log("Hello Java Script :");
+
+let name ="Naseeha";
+let num = 10;
+let decimalNum = 8.6;
+let bln = true;
+
+console.log(num);
+console.log(typeof num);
+console.log(decimalNum);
+console.log(typeof decimalNum)
+console.log(bln);
+console.log(typeof bln);
+console.log(name);
+console.log(typeof name); 
+
+let customers = [];
+let customer={};
+console.log(customer);
+console.log(typeof customer);
+console.log(customers);
+console.log(typeof customers);
+
+alert("hi");
+
+let x = 10;
+let y = "10";
+console.log(x === y);
+
+if(x > y){
+    console.log("x is grater than y");
+}else if(x < y){
+    console.log("x is less than y");
+}else{
+    console.log("x is eyqual to y");
+}
